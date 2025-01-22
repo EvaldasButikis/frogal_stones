53a152695a0173b77aa3ac88f9f7d5fd9043575d
Dana <djerad025@gmail.com>
2025-01-22 12:44:58 +0100

    Slightly editted graph (for labels)

--- punkrockmetal_graph.R
+++ punkrockmetal_graph.R
@@ -1,26 +1,27 @@
 library ('tidyverse')
 
-data_rockmetal <- read_csv('startyear_rock_punk_metal.csv')|>
+data_several <- read_csv('startyear_rock_punk_metal.csv')|>
   pivot_longer( 
     cols = c(rock, punk, metal),
     names_to = "genre",
     values_to = "number of musicians")
 
-ggplot(data = data_rockmetal) +
+ggplot(data = data_several) +
   theme_light() +
   aes(x = year,
       y = `number of musicians`, 
       group = `genre`,
       color = `genre`)+
+  labs(
+    x = "Decades",
+    y = "Number of Musicians",
+    color = "Genre"
+  )+
   scale_x_continuous(
     limits = c(1940, 2010),
     breaks = seq(1940, 2020, 10)
     )+
     geom_point(size = 2)+
     geom_line(size = 1)
-    
-
-
-print(data_rockmetal)  
   
 
