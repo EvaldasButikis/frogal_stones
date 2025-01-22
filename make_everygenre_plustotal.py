7e63c16b20f361e83f9c0e30ccf7aa6b17a6a0f7
Yuna Luisetto Vanbergen <yunamarbay@gmail.com>
2025-01-22 15:34:10 +0100

    good code to create the right data

--- make_everygenre_plustotal.py
+++ make_everygenre_plustotal.py
@@ -10,12 +10,9 @@ with open("clean_data.json", encoding = 'utf-8') as file:
 dictio_per_year = {}
 
 #inputing which genres you want to look at: 
-genre1 = input("What is the first genre?")
-genre2 = input("What is the second genre?")
-genre1 = genre1.lower()
-genre2 = genre2.lower()
+genres_of_interest = ["rock", "punk"]
 #inputing csv name.csv
-csvname = input("What should the csv be called?")
+csvname = "test.csv"
 
 for person in content.values():
     year = person["start_year"]
@@ -25,10 +22,10 @@ for person in content.values():
          continue
     if year not in dictio_per_year:
         dictio_per_year[year] = {
-            "total" : 0,
-            genre1: 0,
-            genre2: 0
+            "total" : 0
         }
+        for genre in genres_of_interest:
+            dictio_per_year[year][genre] = 0
 
     dictio_per_year[year]["total"]+=1
 
@@ -40,12 +37,12 @@ for person in content.values():
 #adding years to dictionary
     
     #adding numbers for each genre to the dictionary
-    for genre in person["genre"]:
-        genre = genre.lower()
-        if genre.__contains__(f"{genre1}"):
-            dictio_per_year[year][f"{genre1}"]+=1
-        if genre.__contains__(f"{genre2}"):
-            dictio_per_year[year][f"{genre2}"]+=1
+    if type(person["genre"]) is not list:
+        person["genre"] = [person["genre"]]
+
+    for genre in genres_of_interest:
+        if any( genre in genry.lower() for genry in person["genre"]):
+            dictio_per_year[year][genre] += 1
   
 
 output = []
@@ -56,7 +53,7 @@ for year, by_genre in dictio_per_year.items():
     output.append(line_dict)
 print(output)
 
-with open(f"{csvname}", 'w') as csvfile:
+with open("good_punk_rock_total_data", 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=output[0].keys(), lineterminator="\n")
     writer.writeheader()
     writer.writerows(output)
