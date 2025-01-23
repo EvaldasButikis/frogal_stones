25752e885e0e433eca1f8fb7a3754c021f7266fd
ShubhiNull <shoe.n.bee@gmail.com>
2025-01-23 14:34:05 +0100

    Final dataset all genres creation file

--- genre_per_year.py
+++ genre_per_year.py
@@ -10,11 +10,7 @@ with open("clean_data.json", encoding = 'utf-8') as file:
 genre_per_year = {}
 
 #inputing which genres you want to look at: 
-<<<<<<< HEAD
-genres_of_interest = [ "blue","punk", "post-hardcore", "metal"]
-=======
 genres_of_interest = ["rock", "punk", "blues", "metal"]
->>>>>>> d72ee51e605d3e0a552f49ccc3bf42a4851e244b
 
 #for every person in the muscian database
 for person in every_musician.values():
@@ -27,12 +23,7 @@ for person in every_musician.values():
     #adding a key for every year in the dictionary
     if year not in genre_per_year:
         genre_per_year[year] = {
-            "total" : 0,
-<<<<<<< HEAD
-            "rock":0
-=======
-            "other":0
->>>>>>> d72ee51e605d3e0a552f49ccc3bf42a4851e244b
+            "total" : 0
         }
     #adding a key for every genre of interest under every year
         for genre in genres_of_interest:
@@ -46,18 +37,12 @@ for person in every_musician.values():
     
     def check_genre(target_genre):
         return any(target_genre in genre.lower() for genre in person["genre"])
-
-<<<<<<< HEAD
-    #If any of the genres in the list mention target genre, add 1 to the corresponding key in the dictionary
     
-    if any("punk" in genre.lower() for genre in person["genre"]):
-        genre_per_year[year]["punk"]+=1
-        
-    
-    for genre in genres_of_interest:
-        if any(genre in genres.lower() for genres in person["genre"]):
-            genre_per_year[year][genre] += 1
-=======
+    if check_genre("rock") and check_genre("punk") and check_genre("metal") and check_genre("blues"): 
+        genre_per_year[year]["rock"]+=1/4
+        genre_per_year[year]["punk"]+=1/4
+        genre_per_year[year]["metal"]+=1/4
+        genre_per_year[year]["blues"]+=1/4
     if check_genre("punk") and check_genre("rock") and check_genre("metal"):
         genre_per_year[year]["rock"]+=1/3
         genre_per_year[year]["punk"]+=1/3
@@ -65,6 +50,12 @@ for person in every_musician.values():
     elif check_genre("blues") and check_genre("rock"): 
         genre_per_year[year]["blues"] +=0.5
         genre_per_year[year]["rock"] +=0.5
+    elif check_genre("blues") and check_genre("metal"):
+        genre_per_year[year]["blues"] +=0.5
+        genre_per_year[year]["metal"] +=0.5
+    elif check_genre("punk") and check_genre("blues"):
+        genre_per_year[year]["blues"] +=0.5
+        genre_per_year[year]["punk"]+=0.5
     elif check_genre("punk") and check_genre("rock"): 
         genre_per_year[year]["rock"]+=0.5
         genre_per_year[year]["punk"]+=0.5
@@ -84,7 +75,6 @@ for person in every_musician.values():
         genre_per_year[year]["blues"] +=1
   
     
->>>>>>> d72ee51e605d3e0a552f49ccc3bf42a4851e244b
 
 #Turning dictionary of dictionaries into list of dictionaries
 output = []
@@ -92,15 +82,11 @@ for year, by_genre in genre_per_year.items():
     line_dict = {"year": year}
     for key in by_genre:
         line_dict[key] = by_genre[key]
-    output.append(line_dict) 
+    output.append(line_dict)
 print(output)
 
 #outputting to csv
-<<<<<<< HEAD
-with open("Data_rock_blues_post-hardcore_punk_metal.csv", 'w') as csvfile:
-=======
 with open("Data_rock_punk_metal_blues.csv", 'w') as csvfile:
->>>>>>> d72ee51e605d3e0a552f49ccc3bf42a4851e244b
     writer = csv.DictWriter(csvfile, fieldnames=output[0].keys(), lineterminator="\n")
     writer.writeheader()
     writer.writerows(output)
