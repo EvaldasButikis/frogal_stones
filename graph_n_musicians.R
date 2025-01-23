85a1d49002c29e6ecf9aa9ad60b039753fb27385
Dana <djerad025@gmail.com>
2025-01-23 17:19:01 +0100

    Updated Graph scripts for n_musicians and pr_total + the graphs themselves!!

--- graph_n_musicians.R
+++ graph_n_musicians.R
@@ -1,13 +1,12 @@
 library ('tidyverse')
 
-data_n_genres <- read_csv('Data_rock_punk_blues_metal.csv')|>
+data_finals <- read_csv('Data_final.csv')|>
   pivot_longer( 
-    cols = c(rock, total),
+    cols = c(Rock, Punk, Metal, total),
     names_to = "genre",
     values_to = "number of musicians")
-print(data_n_genres)
 
-ggplot(data = data_n_genres) +
+ggplot(data = data_finals) +
   theme_light() +
   aes(x = year,
       y = `number of musicians`, 
@@ -27,20 +26,21 @@ ggplot(data = data_n_genres) +
   )+
   scale_color_discrete(
     name = "Genre"
+    #,
+  #  labels = c("Garage Rock","Post Hardcore", "Punk")
   )+
   scale_x_continuous(
     limits = c(1940, 2010),
     breaks = seq(1940, 2020, 10)
     )+
-    geom_point(size = 2)+
-    geom_line(size = 1)+
-    scale_color_manual(values = c("#F8766D", "#000000")
-              ,
-                   labels = c( "Rock", "All genres")
-      )
+  geom_point(size = 2) +
+  geom_line(size = 1) +
+  scale_color_manual(
+    values = c("#C77CFF", "#7CAE00", "#F8766D", "#000000"),
+    labels = c("Metal", "Punk", "Rock", "All")
+    )
 
-#+
-#  ggsave('n_musicians_rock_total.pdf')
+ +    ggsave('n_musicians_rock_punk_metal_total.pdf')
   
 
 
--- graph_pr_total.R
+++ graph_pr_total.R
@@ -1,13 +1,13 @@
 library ('tidyverse')
 library(ggplot2)
 
-data_pr_genres <- read_csv('Data_rock_punk_blues_metal.csv')|>
+data_prpunk <- read_csv('Punk_subgenres.csv')|>
   pivot_longer( 
-    cols = c(blues, rock),
+    cols = c(`garage rock`, `post_hardcore`, `punk`),
     names_to = "genre",
     values_to = "number of musicians")
 
-ggplot(data = data_pr_genres) +
+ggplot(data = data_prpunk) +
   theme_light() +
   aes(x = year,
       y = `number of musicians`/ total, 
@@ -26,21 +26,20 @@ ggplot(data = data_pr_genres) +
     axis.text = element_text(size = 12)
     )+
   scale_color_discrete(
-    name = "Genre"
-    )+
+    name = "Genre",
+  labels = c("Garage Rock","Post Hardcore", "Punk")
+)+
   scale_x_continuous(
-    limits = c(1900, 2010),
-    breaks = seq(1900, 2020, 10)
+    limits = c(1950, 2010),
+    breaks = seq(1950, 2010, 10)
   )+
   scale_y_continuous(
     labels = scales::label_percent()
     )+
   geom_point(size = 2)+
-  geom_line(size = 1)+
-  scale_color_manual(values = c("#7CAE00","#C77CFF"),
-                     labels = c( "Metal","Punk")
-                     )
-
-#+
-#ggsave('pr_total_rock_metal_punk.pdf')
+  geom_line(size = 1)
+#+ scale_color_manual(values = c("#00BFC4", "#F8766D"),
+#                     labels = c("Blues", "Rock")
+            #         )
++    ggsave('pr_total_punk_sub.pdf')
 
